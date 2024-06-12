606b1974e1d1f57ffd3ec94270ccaa2e662038be
Abhishek Diwakar <abhishek88666kumar@gmail.com>
2024-06-12 11:26:17 +0530

    Added Opt-in/out instruction for WhereDidISpend on WhatsApp

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -84,4 +84,145 @@ export class AppController {
       `,
     };
   }
+
+  @Get('v/whatsapp')
+  @Render('whatsapp-index')
+  whatsappIndex() {
+    return {};
+  }
+
+  @Get('v/whatsapp/opt-in-out-instructions')
+  @Render('whatsapp-template')
+  whatsappSupport() {
+    return {
+      title: 'WhereDidISpend on Whatsapp: Opt-In/Out Instructions',
+      description:
+        'Instructions for Opting in and out of WhereDidISpend services offered on Whatsapp',
+      content: `<section>
+        <h2>About WhereDidISpend on WhatsApp</h2>
+        <p>By Opting-In for this feature, users of WhereDidISpend app can share
+          a screenshot of their UPI payment to our WhatsApp business account and
+          we'll automatically add transaction details in your WhereDidISpend app
+          account.</p>
+      </section>
+      <hr />
+      <section id='opt-in'>
+        <h3>Opt-In</h3>
+        <p>Here's how to Opt-In for this feature from WhereDidISpend mobile app:</p>
+        <div class='row'>
+          <div class='col-12 mt-4 col-md-4 text-center'>
+            <h4>Step 1: Open App Settings</h4>
+            <p>Open
+              <strong>WhereDidISpend</strong>
+              app in your smartphone and tap on your profile picture.</p>
+            <img
+              src='/static-assets/screenshots/whatsapp-feature/1.png'
+              class='screenshot'
+            />
+          </div>
+          <div class='col-12 mt-4 col-md-4 text-center'>
+            <h4>Step 2: Tap on "Add via WhatsApp"</h4>
+            <p>Tap on the "Add via WhatsApp" to open Opt-In/Out screen.</p>
+            <img
+              src='/static-assets/screenshots/whatsapp-feature/2.png'
+              class='screenshot'
+            />
+          </div>
+          <div class='col-12 mt-4 col-md-4 text-center'>
+            <h4>Step 3: Toggle "Enable"</h4>
+            <p>Toggle the "Enable" switch to start Opt-In process. You will be
+              asked to enter your WhatsApp number.</p>
+            <img
+              src='/static-assets/screenshots/whatsapp-feature/3.png'
+              class='screenshot'
+            />
+          </div>
+          <div class='col-12 mt-4 col-md-4 text-center'>
+            <h4>Step 4: Enter you WhatsApp number</h4>
+            <p>Enter your WhatsApp number from which you'll send us your UPI
+              payment screenshots.</p>
+            <img
+              src='/static-assets/screenshots/whatsapp-feature/4.png'
+              class='screenshot'
+            />
+          </div>
+          <div class='col-12 mt-4 col-md-4 text-center'>
+            <h4>Step 5: Send OTP</h4>
+            <p>Tap on "Send OTP on WhatsApp" button to receive an OTP on your
+              WhatsApp number for verification.</p>
+            <img
+              src='/static-assets/screenshots/whatsapp-feature/5.png'
+              class='screenshot'
+            />
+          </div>
+          <div class='col-12 mt-4 col-md-4 text-center'>
+            <h4>Step 6: Verify OTP</h4>
+            <p>Enter received OTP and tap on the "Verify OTP" button.</p>
+            <img
+              src='/static-assets/screenshots/whatsapp-feature/6.png'
+              class='screenshot'
+            />
+          </div>
+          <div class='col-12 mt-4 col-md-4 text-center'>
+            <h4>Step 7: Done</h4>
+            <p>You've successfully opted-in for this feature.</p>
+            <img
+              src='/static-assets/screenshots/whatsapp-feature/7.png'
+              class='screenshot'
+            />
+          </div>
+        </div>
+      </section>
+      <hr />
+      <section id='opt-out' class='mt-5'>
+        <h3>Opt-Out</h3>
+        <p>Here's how to Opt-Out of this feature from WhereDidISpend mobile app:</p>
+        <div class='row'>
+          <div class='col-12 mt-4 col-md-4 text-center'>
+            <h4>Step 1: Open App Settings</h4>
+            <p>Open
+              <strong>WhereDidISpend</strong>
+              app in your smartphone and tap on your profile picture.</p>
+            <img
+              src='/static-assets/screenshots/whatsapp-feature/1.png'
+              class='screenshot'
+            />
+          </div>
+          <div class='col-12 mt-4 col-md-4 text-center'>
+            <h4>Step 2: Tap on "Add via WhatsApp"</h4>
+            <p>Tap on the "Add via WhatsApp" to open Opt-In/Out screen.</p>
+            <img
+              src='/static-assets/screenshots/whatsapp-feature/2.png'
+              class='screenshot'
+            />
+          </div>
+          <div class='col-12 mt-4 col-md-4 text-center'>
+            <h4>Step 3: Toggle "Enable"</h4>
+            <p>Toggle the "Enable" switch to start Opt-Out process. An alert
+              will pop up.</p>
+            <img
+              src='/static-assets/screenshots/whatsapp-feature/7.png'
+              class='screenshot'
+            />
+          </div>
+          <div class='col-12 mt-4 col-md-4 text-center'>
+            <h4>Step 4: Opt-Out</h4>
+            <p>Read the warning carefully and tap on Opt-Out.</p>
+            <img
+              src='/static-assets/screenshots/whatsapp-feature/8.png'
+              class='screenshot'
+            />
+          </div>
+          <div class='col-12 mt-4 col-md-4 text-center'>
+            <h4>Step 5: Done</h4>
+            <p>You've successfully opted-out of this feature.</p>
+            <img
+              src='/static-assets/screenshots/whatsapp-feature/3.png'
+              class='screenshot'
+            />
+          </div>
+        </div>
+      </section>`,
+    };
+  }
 }
